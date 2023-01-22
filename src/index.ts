caafebf4f7466b617a1819b5e7ae7e1c16bfe4bd
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-01-22 12:33:43 +0400

    add length example

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,15 @@ class Test {
         const arr = this.word.toLocaleLowerCase().split('').reverse();
         return (arr.join('') === this.word.toLocaleLowerCase());
     }
+
+    public length() { 
+        return this.word.length;
+    }
 }
 
 
 const palindrome = new Test("Anna");
 const isPalindrome = palindrome.check();
-console.log(isPalindrome)
+console.log(isPalindrome);
+const wordLength = palindrome.length();
+console.log(wordLength);
